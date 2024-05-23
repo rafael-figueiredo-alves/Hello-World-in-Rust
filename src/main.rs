2f80bc0abb8ccd83c9ebe1c956a8482d291728d9
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2024-05-23 20:56:48 -0300

    Pequena alteração no app
    Adição da pergunta da idade e adição da mesma informação no retorno. Apenas para praticar o que aprendi.

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,20 @@ use std::io;
 fn main() {
     println!("Olá mundo");
     println!("Este é meu primeiro app em Rust");
-    println!("Versão 1.0");
+    println!("Versão 1.1.0");
     println!("");
     println!("Informe seu nome:");
 
     let mut name = String::new();
     io::stdin().read_line(&mut name).expect("Ocorreu um erro"); 
 
-    println!("Seu nome é {}", name);
+    print!("");
+    println!("Informe sua idade:");
+
+    let mut age: String = String::new();
+    io::stdin().read_line(&mut age).expect("Idade é inválida");
+
+    println!("Seu nome é {} e você tem {} anos", name, age);
     println!("");
 
     println!("Pressione qualquer tecla para fechar a janela...");
